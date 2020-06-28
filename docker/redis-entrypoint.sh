274a97703c8e7a7b9f2956d9f07b64e5a1b7232a
Ryan Northey <ryan@synca.io>
2020-06-28 06:41:24 +0100

    build

--- docker/redis-entrypoint.sh
+++ docker/redis-entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-echo "Starting redis on socket: $REDIS_SOCKET"
-chown -R redis:redis `dirname $REDIS_SOCKET`
-
-# if the command ends with --unixsocket, then add $REDIS_SOCKET
+# if the command ends with --unixsocket, then add $REDIS_SOCKET and uid
 for last; do true; done
 if [ $last = '--unixsocket' ]; then
-    exec docker-entrypoint.sh $@ $REDIS_SOCKET
+    echo "Starting redis on socket: $REDIS_SOCKET"
+    chown -R redis:redis `dirname $REDIS_SOCKET`
+    exec docker-entrypoint.sh $@ $REDIS_SOCKET --unixsocketperm $REDIS_UID
 else
     exec docker-entrypoint.sh $@
 fi
